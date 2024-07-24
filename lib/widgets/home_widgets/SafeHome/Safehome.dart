6bef841ef61657f87d06f19a063f435a14c02c31
Meetj28 <meetj.jain576@gmail.com>
2024-07-24 15:58:40 +0530

    Add share location pop up

--- lib/widgets/home_widgets/SafeHome/Safehome.dart
+++ lib/widgets/home_widgets/SafeHome/Safehome.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:women_safety_app/components/PrimaryButton.dart';
 
-class SafeHome extends StatelessWidget {
+class SafeHome extends StatefulWidget {
+
+
+  @override
+  State<SafeHome> createState() => _SafeHomeState();
+}
+
+class _SafeHomeState extends State<SafeHome> {
+
+  _getPermission() async => await [Permission.sms].request;
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  _sendSms(String phoneNumber, String message, {int? simSlot}) async{
+    await BackgroundSms.sendMessage(
+
+    )
+
+  }
   void showModalSafeHome(BuildContext context) {
     showModalBottomSheet(
       context: context,
       builder: (context) {
         return Container(
           height: MediaQuery.of(context).size.height / 1.4,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+              Text(
+                "Send your current location immediately to your emergency contancts",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 10),
+              Primarybutton(title: "Get Location", onPressed: () {}),
+              SizedBox(height: 10),
+              Primarybutton(title: "Send Alert", onPressed: () {}),
+            ],
+            ),
+          ),
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(20),
